3a52a61c9d9820fe1e458d0327a11342bddbd70f
KustoSan <kustosan@gmail.com>
2015-05-08 19:20:01 +0200

    Cleaning

--- js/compare.js
+++ js/compare.js
@@ -1,74 +1,60 @@
-
-
 // Get URL vars
 function getUrlVars() {
     var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        vars[key] = value;
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+      vars[key] = value;
     });
     return vars;
-}
+  }
+  // Start everything if user is set
+$(document).ready(function() {
 
+  $.ajaxSetup({
+    cache: false
+  });
+  var user = getUrlVars()["user"];
+  var page = 1;
+  if (user != undefined) {
+    $('#updateBtn').html('<i class="fa fa-spinner fa-pulse"></i> Loading compatibilities...');
+    $('.gprogress').html(user + "'s followed users anime taste compatibility")
 
-// Start everything if user is set
-$(document).ready(function () {
+    do {
+      // Get follow json
+      $.jsonp({
+        url: 'https://hummingbird.me/users?callback=?&followed_by=' + user + '&page=' + page, // any JSON endpoint
+        jsonpSupport: true, // if URL above supports JSONP (optional)
+        success: function(data) {
 
-	$.ajaxSetup({ cache: false });
+          // Get each follower compatibility
+          jQuery.each(data.users, function(a, val) {
 
-    var user = getUrlVars()["user"];
-    var page = 1;
+            $.getJSON('https://hbird-cmp-node.herokuapp.com/compatibility/anime?user1=' + user + '&user2=' + val.id + '&callback=asd', function(data) {
 
-	if (user != undefined) {
-		$('#updateBtn').html('<i class="fa fa-spinner fa-pulse"></i> Loading compatibilities...');
-		$('.gprogress').html(user + "'s followed users anime compatibility")
- 
-		do 
-		{
-			// Get follow json
-	       	$.jsonp({
-			    url: 'https://hummingbird.me/users?callback=?&followed_by=' + user + '&page=' + page, // any JSON endpoint
-			    jsonpSupport: true, // if URL above supports JSONP (optional)
-			    success: function (data){
-			      
-			      	// Get each follower compatibility
-		        	jQuery.each(data.users, function (a, val) {
-
-		  				$.getJSON('https://hbird-cmp-node.herokuapp.com/compatibility/anime?user1=' + user + '&user2=' + val.id + '&callback=asd', function (data) {
-
-		            		$('#members').append('<tr id="ded"><td class="countTd"><b></b></td><td><a style="color:#428bca;" href="https://hummingbird.me/users/' + val.id + '" target="_blank"</a>' + val.id + '</td><td>' + data.percent + '</td><td>Followed</td></tr>');
-	                        $('th').removeAttr("data-sorted")
-	                        $('th').removeAttr("data-sorted-direction")
-	                        Sortable.init();
-
-					        for (var i = 0; i <= $('.countTd').length; i++) {
-								$('.countTd:eq(' + i + ')').html('#' + (i + 1) + ' |');	
-							};
-		            	})
-
-		            	.done( function() {
-
-		            	})
-		        	})
-			    }
-			});
-
-	        page++
-
-	    } 
-	    while (page != "100")
-	}
+              $('#members').append('<tr id="ded"><td class="countTd"><b></b></td><td><a style="color:#428bca;" href="https://hummingbird.me/users/' +
+                val.id + '" target="_blank"</a>' + val.id + '</td><td>' + data.percent + '</td><td>Followed</td></tr>');
+              $('th').removeAttr("data-sorted")
+              $('th').removeAttr("data-sorted-direction")
+              Sortable.init();
+              for (var i = 0; i <= $('.countTd').length; i++) {
+                $('.countTd:eq(' + i + ')').html('#' + (i + 1) + ' |');
+              };
+            })
+          })
+        }
+      });
+      page++
+    }
+    while (page != "100")
+  }
 });
 
-
-
 // When all Ajax requests stop
-$(document).ajaxStop(function () {
+$(document).ajaxStop(function() {
 
-	$("#updateBtn").html('<i class="fa fa-check"></i> Enjoy! ^o^');
-					            
-	$(".dedo").click(function() {
-		for (var i = 0; i <= $('.countTd').length; i++) {
-			$('.countTd:eq(' + i + ')').html('#' + (i + 1) + ' |');	
-		}
-	})
-})
+  $("#updateBtn").html('<i class="fa fa-check"></i> Enjoy! ^o^');
+  $(".dedo").click(function() {
+    for (var i = 0; i <= $('.countTd').length; i++) {
+      $('.countTd:eq(' + i + ')').html('#' + (i + 1) + ' |');
+    }
+  })
+});
--- js/hb-users.js
+++ js/hb-users.js
@@ -3,74 +3,76 @@
 
 /*################ Get url vars ################*/
 function getUrlVars() {
-    var vars = {};
-    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
-        vars[key] = value;
-    });
-    return vars;
+  var vars = {};
+  var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
+    vars[key] = value;
+  });
+  return vars;
 }
 
 
 /*################ Get user by url var ################*/
-$(document).ready(function () {
+$(document).ready(function() {
 
-	$.ajaxSetup({ cache: false });
+  $.ajaxSetup({
+    cache: false
+  });
 
-    $("#search").ready(function () {
+  $("#search").ready(function() {
 
-        var user = getUrlVars()["user"];
-        var status = getUrlVars()["status"];
+    var user = getUrlVars()["user"];
+    var status = getUrlVars()["status"];
 
-        if (user == undefined || user == "") {
-            user = "kusto";
+    if (user == undefined || user == "") {
+      user = "kusto";
+    }
+
+    if (status == undefined || status == "" || status == "undefined") {
+      status = "completed";
+    }
+
+    $.jsonp({
+      url: 'http://hummingbird.me/api/v1/users/' + user + '?callback=?', // any JSON endpoint
+      jsonpSupport: true, // if URL above supports JSONP (optional)
+      success: function(data) {
+        document.getElementById('usersearch').value = data.name;
+        if (data.waifu == null) {
+          data.waifu = "Unknown"
+        }
+        if (data.location == null) {
+          data.location = "Unknown"
+        }
+        if (data.website == null) {
+          data.website = "Unknown"
+        }
+        $('#h1-library').append(data.name + "'s " + '<span class="label label-primary">' + status + '</span>' + " anime library");
+        $('#hb-header').html('<div class="hb-cover cover-opacity"></div><div class="hb-cover" style="background-image: url(' + data.cover_image + ')"</div>');
+        $('#hb-header').append('<img class="hb-avatar" ' + 'src=' + '"' + data.avatar + '"' + '</img>');
+        $('#hb-biobox').append('<p class="hb-username"><a target="_blank" href=' + '"' + '//hummingbird.me/users/' + data.name + '"' + '>' + data.name + '</a></p>');
+        $('#hb-biobox').append('<p class="hb-bio">' + data.bio + '</p>');
+        $('#hb-info').append('<i class="fa fa-heart waifu"></i><p class="hb-waifu-husbando">' + data.waifu + ' </p>');
+        $('#hb-info').append('<i class="fa fa-home home"></i><p class="hb-location"> ' + data.location + '</p>');
+        $('#hb-info').append('<p class="hb-website"><i class="fa fa-link"></i> ' + data.website + '</p>');
+      }
+    })
+
+    $.jsonp({
+      url: 'http://hummingbird.me/api/v1/users/' + user + '/library?status=' + status, // any JSON endpoint
+      jsonpSupport: true, // if URL above supports JSONP (optional)
+      success: function(library) {
+
+        if (jQuery.isEmptyObject(library)) {
+          $('#h1-library').html("<h1>This library is empty D:")
         }
 
-        if (status == undefined || status == "" || status == "undefined") {
-            status = "completed";
+        var i = 0;
+        if (library[i] != undefined) {
+          while (library[i] != undefined) {
+            $('#hb-library').append('<div class="col-md-3 column"><a target="_blank" href="' + library[i].anime.url + '"><figure class="g-figures"><img class="g-images" alt="1920x1080" src="' + library[i].anime.cover_image + '"><figcaption class="g-figcaptions">' + library[i].anime.title + '</figcaption></a></figure></a></div>');
+            i++;
+          }
         }
-
-		$.jsonp({
-				url: 'http://hummingbird.me/api/v1/users/' + user + '?callback=?', // any JSON endpoint
-				jsonpSupport: true, // if URL above supports JSONP (optional)
-				success: function (data){
-		            document.getElementById('usersearch').value = data.name;
-		            if (data.waifu == null) {
-		                data.waifu = "Unknown"
-		            }
-		            if (data.location == null) {
-		                data.location = "Unknown"
-		            }
-		            if (data.website == null) {
-		                data.website = "Unknown"
-		            }
-		            $('#h1-library').append(data.name + "'s " + '<span class="label label-primary">' + status + '</span>' + " anime library");
-		            $('#hb-header').html('<div class="hb-cover cover-opacity"></div><div class="hb-cover" style="background-image: url(' + data.cover_image + ')"</div>');
-		            $('#hb-header').append('<img class="hb-avatar" ' + 'src=' + '"' + data.avatar + '"' + '</img>');
-		            $('#hb-biobox').append('<p class="hb-username"><a target="_blank" href=' + '"' + '//hummingbird.me/users/' + data.name + '"' + '>' + data.name + '</a></p>');
-		            $('#hb-biobox').append('<p class="hb-bio">' + data.bio + '</p>');
-		            $('#hb-info').append('<i class="fa fa-heart waifu"></i><p class="hb-waifu-husbando">' + data.waifu + ' </p>');
-		            $('#hb-info').append('<i class="fa fa-home home"></i><p class="hb-location"> ' + data.location + '</p>');
-		            $('#hb-info').append('<p class="hb-website"><i class="fa fa-link"></i> ' + data.website + '</p>');
-		        }
-		})
-
-		$.jsonp({
-				url: 'http://hummingbird.me/api/v1/users/' + user + '/library?status=' + status, // any JSON endpoint
-				jsonpSupport: true, // if URL above supports JSONP (optional)
-				success: function (library){
-
-		            if (jQuery.isEmptyObject(library)) {
-		                $('#h1-library').html("<h1>This library is empty D:")
-		            }
-
-		            var i = 0;
-		            if (library[i] != undefined) {
-		                while (library[i] != undefined) {
-		                    $('#hb-library').append('<div class="col-md-3 column"><a target="_blank" href="' + library[i].anime.url + '"><figure class="g-figures"><img class="g-images" alt="1920x1080" src="' + library[i].anime.cover_image + '"><figcaption class="g-figcaptions">' + library[i].anime.title + '</figcaption></a></figure></a></div>');
-		                    i++;
-		                }
-		            }
-	        	}
-        })    	
-	})
+      }
+    })
+  })
 });
