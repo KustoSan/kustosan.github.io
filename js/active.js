d37300a399c62d50f98f129d7edba86f175d9000
KustoSan <kustosan@gmail.com>
2015-05-18 01:17:10 +0200

    Navbar redesign + other stuff
    Changed all scripts calls before body end tag.
    Navbar redesign.
    Active class semi-automation.
    Home site-branding positioning fix.

---
+++ js/active.js
@@ -0,0 +1,17 @@
+var url = window.location.pathname;
+var filename = url.substring(url.lastIndexOf('/') + 1);
+
+i = 0;
+while (i != 100) {
+  href = $('a:eq(' + i + ')').attr('href')
+  if (href == filename) {
+    $('a:eq(' + i + ')').parent('li').attr('class', 'active')
+    console.log(filename)
+  }
+  i++
+}
+
+$('.navbar-brand').click(function(event) {
+  // stop the click on the link adding a # to the end of the
+  event.preventDefault();
+});
