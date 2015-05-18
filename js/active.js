b145fcc4aec417a8b7b191c156e2e423015609bb
Kusto <kustosan@gmail.com>
2015-05-18 10:58:06 +0200

    Finished active.js

--- js/active.js
+++ js/active.js
@@ -2,10 +2,10 @@ var url = window.location.pathname;
 var filename = url.substring(url.lastIndexOf('/') + 1);
 
 i = 0;
-while (i != 100) {
+while (i != $('a').length) {
   href = $('a:eq(' + i + ')').attr('href')
   if (href == filename) {
-    $('a:eq(' + i + ')').parent('li').attr('class', 'active')
+    $('a:eq(' + i + ')').parents('li').addClass('active')
     console.log(filename)
   }
   i++
