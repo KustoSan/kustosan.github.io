71b36be9ffb6d39877ee236112b3e5b5430e2e24
KustoSan <kustosan@gmail.com>
2015-06-11 03:03:32 +0200

    Change gallery image names + javascript

--- js/gallery.js
+++ js/gallery.js
@@ -1,10 +1,39 @@
-$.getJSON('//api.github.com/repos/kustosan/kustosan.github.io/contents/css/imgs', function(data) {
+$(document).ready(function() {
+  var numb
 
-  jQuery.each(data, function(i, image) {
+  jQuery.get('css/imgs/files.txt', function(data) {
 
-    $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + image.path + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
-      image.path.replace('imgs', 'thumbnails').replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p>Under testing</p></div></div></a></div>')
+    var numb = data;
+    var images = []
 
-  })
+    for (var i = numb; i >= 0; i--) {
+      images.push(i + '.png');
+    }
 
-});
+    jQuery.each(images, function(i, image) {
+
+      var tempImage1 = new Image();
+      tempImage1.src = 'css/imgs/' + image;
+      tempImage1.onload = function() {
+        $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
+      }
+
+      $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + 'css/imgs/' + image + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
+        'css/thumbnails/' + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '"></p></div></div></a></div>')
+
+    })
+
+    $(".lazy").lazyload({
+      effect: "fadeIn",
+      threshold: 800
+    });
+    $('.popup, .mfp-img').magnificPopup({
+      type: 'image'
+    });
+    $("img").error(function hide() {
+      $(this).hide();
+    });
+
+  });
+
+})
