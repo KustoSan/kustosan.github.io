8eae1eb2806aeb47fb1b3b0c856d448afa8f3e05
KustoSan <kustosan@gmail.com>
2015-06-11 15:53:11 +0200

    Change image hosting to Google Drive

--- js/gallery.js
+++ js/gallery.js
@@ -1,38 +1,35 @@
 $(document).ready(function() {
 
-  jQuery.get('css/imgs/files.txt', function(data) {
+  var images = []
+  for (var i = 105; i >= 0; i--) {
+    images.push(i + '.png');
+  }
 
-    var numb = data;
-    var images = []
+  original = 'https://googledrive.com/host/0B4CDzpF5TN3gfnZmcTc4QmNqeVFJY0ZoWlhJQVR6MGh3U0hnbjRLMWJaRXFrSlBnVTlIWEk/original/'
+  thumbnails = 'https://googledrive.com/host/0B4CDzpF5TN3gfnZmcTc4QmNqeVFJY0ZoWlhJQVR6MGh3U0hnbjRLMWJaRXFrSlBnVTlIWEk/thumbnails/'
 
-    for (var i = numb; i >= 0; i--) {
-      images.push(i + '.png');
-    }
+  jQuery.each(images, function(i, image) {
 
-    jQuery.each(images, function(i, image) {
+    $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + original + image + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
+        thumbnails + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '">Under testing</p></div></div></a></div>')
+      /*
+            var tempImage1 = new Image();
+            tempImage1.src = 'css/imgs/' + image;
+            tempImage1.onload = function() {
+              $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
+            }
+      */
+  })
 
-      $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + 'css/imgs/' + image + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
-        'css/thumbnails/' + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '">Under testing</p></div></div></a></div>')
-/*
-      var tempImage1 = new Image();
-      tempImage1.src = 'css/imgs/' + image;
-      tempImage1.onload = function() {
-        $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
-      }
-*/
-    })
-
-    $(".lazy").lazyload({
-      effect: "fadeIn",
-      threshold: 800
-    });
-    $('.popup, .mfp-img').magnificPopup({
-      type: 'image'
-    });
-    $("img").error(function hide() {
-      $(this).hide();
-    });
-
+  $(".lazy").lazyload({
+    effect: "fadeIn",
+    threshold: 800
+  });
+  $('.popup, .mfp-img').magnificPopup({
+    type: 'image'
+  });
+  $("img").error(function hide() {
+    $(this).hide();
   });
 
 })
