6fc3cec57945e337dfcf2d696825d8ba9e477972
KustoSan <kustosan@gmail.com>
2015-06-11 21:34:58 +0200

    Add fixed image resolution

--- js/gallery.js
+++ js/gallery.js
@@ -11,14 +11,8 @@ $(document).ready(function() {
   jQuery.each(images, function(i, image) {
 
     $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + original + image + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
-        thumbnails + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '">Under testing</p></div></div></a></div>')
-      /*
-            var tempImage1 = new Image();
-            tempImage1.src = 'css/imgs/' + image;
-            tempImage1.onload = function() {
-              $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
-            }
-      */
+      thumbnails + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '">Loading resolution...</p></div></div></a></div>')
+
   })
 
   $(".lazy").lazyload({
@@ -31,5 +25,15 @@ $(document).ready(function() {
   $("img").error(function hide() {
     $(this).hide();
   });
+
+  jQuery.each(images, function(i, image) {
+
+    var tempImage1 = new Image();
+    tempImage1.src =  original + image;
+    tempImage1.onload = function() {
+      $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
+    }
+
+  })
 
 })
