555e7e694dd324a8b770397a70fdf72d8350ca78
KustoSan <kustosan@gmail.com>
2015-06-11 13:25:15 +0200

    temporary removing image resolutions

--- js/gallery.js
+++ js/gallery.js
@@ -12,14 +12,14 @@ $(document).ready(function() {
     jQuery.each(images, function(i, image) {
 
       $('.row').append('<div class="col-lg-3 col-md-4 col-sm-4 col-xs-6"><a href="' + 'css/imgs/' + image + '" class="popup"><div class="thumbnail gallery-thumbnail"><img class="lazy" data-original="' +
-        'css/thumbnails/' + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '"></p></div></div></a></div>')
-
+        'css/thumbnails/' + image.replace('.png', '.jpg') + '" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7"><div class="caption"><p class="' + i + '">Under testing</p></div></div></a></div>')
+/*
       var tempImage1 = new Image();
       tempImage1.src = 'css/imgs/' + image;
       tempImage1.onload = function() {
         $('.' + i).html(tempImage1.width + 'x' + tempImage1.height);
       }
-
+*/
     })
 
     $(".lazy").lazyload({
