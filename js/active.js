90ad50e3192f00d8b218c7474bee302d37b2170f
KustoSan <kustosan@gmail.com>
2015-08-12 19:59:58 +0200

    Acive tab home fix

--- js/active.js
+++ js/active.js
@@ -7,6 +7,9 @@ while (i != $('a').length) {
   if (href == filename) {
     $('a:eq(' + i + ')').parents('li').addClass('active')
   }
+  if (filename == "") {
+    $('a:eq(1)').parents('li').addClass('active')
+  }
   i++
 }
 
