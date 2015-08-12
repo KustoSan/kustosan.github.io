d1b914ee85e13e359bf38b4384db9e90c56773d0
KustoSan <kustosan@gmail.com>
2015-08-12 19:47:03 +0200

    Add particleground to home page

---
+++ js/particlegound.options.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#F48FB1',
+    lineColor: '#F48FB1'
+  });
+}, false);
